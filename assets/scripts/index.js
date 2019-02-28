a69ce393279310fd0fdbe8540b18d4420b4715e7
David Welsh <welshdav@msu.edu>
2019-02-28 14:35:29 -0600

    Update index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,19 @@
 import {createStore} from 'redux'
+
+function todos(state =[] action){
+  switch (action.type){
+    case 'ADD_TOO':
+      return state.concat([action.text])
+    default:
+      return state
+   }
+} 
+
+const store = createStore(todos, ['Use Redux'})
+    
+store.dispatch({
+  type: 'ADD_TOO',
+  text: 'Read the docs'
+})
+         
+console.log(store.getState())
